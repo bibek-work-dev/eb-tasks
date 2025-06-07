4ab3865064e0a885956ba396e76e14770d438b97
Bibek <www.koiralabibek2058@gmail.com>
2025-06-07 18:50:13 +0545

    finished

--- node-express/dist/src/types/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Usertatus = void 0;
-var Usertatus;
-(function (Usertatus) {
-    Usertatus[Usertatus["Active"] = 0] = "Active";
-    Usertatus[Usertatus["InActive"] = 1] = "InActive";
-})(Usertatus || (exports.Usertatus = Usertatus = {}));
--- node-express/dist/src/utils/apiResponse.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-class ApiResponse {
-    constructor(statusCode = 500) {
-        this.statusCode = statusCode;
-    }
-}
